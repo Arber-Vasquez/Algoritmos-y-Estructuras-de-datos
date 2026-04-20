1ab35712cf1b586f535266d7406361aeb357227c
Arber-Vasquez <arber.vasquez@unas.edu.pe>
2026-04-20 12:10:43 -0500

    Array

---
+++ Semana01/ejer18.java
@@ -0,0 +1,27 @@
+package Semana01;
+
+import java.util.Scanner;
+
+public class ejer18 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[5];
+
+        System.out.println("Ingrese 5 números:");
+
+        for (int i = 0; i < 5; i++) {
+            numeros[i] = sc.nextInt();
+        }
+
+        int suma = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            suma += numeros[i];
+        }
+
+        double promedio = (double) suma / numeros.length;
+
+        System.out.println("Promedio: " + promedio);
+        sc.close();
+    }
+}
