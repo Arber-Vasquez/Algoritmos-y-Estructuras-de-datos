f3cc92dea1f7d1119f247ad25707a02bc934ed8e
Arber-Vasquez <arber.vasquez@unas.edu.pe>
2026-04-20 12:11:54 -0500

    Vector

---
+++ Semana01/ejer21.java
@@ -0,0 +1,15 @@
+package Semana01;
+
+import java.util.Vector;
+
+public class ejer21 {
+    public static void main(String[] args) {
+        Vector<Integer> numeros = new Vector<>();
+
+        numeros.add(10);
+        numeros.add(20);
+        numeros.add(30);
+
+        System.out.println(numeros);
+    }
+}
