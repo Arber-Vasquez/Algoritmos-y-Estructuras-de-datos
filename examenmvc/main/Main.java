203e9f522588546edc931f1647d86f3f2119b090
Arber-Vasquez <arber.vasquez@unas.edu.pe>
2026-04-30 11:19:50 -0500

    Entrega 2 - modelo y vista principal

--- examenmvc/main/Main.java
+++ examenmvc/main/Main.java
@@ -1,7 +1,8 @@
 package examenmvc.main;
-
+import examenmvc.vista.Tarea;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema de Tareas iniciado...");
+        Tarea vista = new Tarea();
+        vista.setVisible(true);
     }
 }
---
+++ examenmvc/vista/Tarea.java
@@ -0,0 +1,59 @@
+package examenmvc.vista;
+import javax.swing.*;
+public class Tarea extends JFrame {
+
+    public JTextField txtCodigo, txtTitulo, txtCurso, txtFecha;
+    public JComboBox<String> cbEstado;
+    public JButton btnRegistrar;
+
+    public Tarea() {
+        setTitle("Sistema de Tareas");
+        setSize(400, 320);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JLabel lblCodigo = new JLabel("Código:");
+        lblCodigo.setBounds(20, 20, 80, 25);
+        add(lblCodigo);
+
+        txtCodigo = new JTextField();
+        txtCodigo.setBounds(120, 20, 150, 25);
+        add(txtCodigo);
+
+        JLabel lblTitulo = new JLabel("Título:");
+        lblTitulo.setBounds(20, 60, 80, 25);
+        add(lblTitulo);
+
+        txtTitulo = new JTextField();
+        txtTitulo.setBounds(120, 60, 150, 25);
+        add(txtTitulo);
+
+        JLabel lblCurso = new JLabel("Curso:");
+        lblCurso.setBounds(20, 100, 80, 25);
+        add(lblCurso);
+
+        txtCurso = new JTextField();
+        txtCurso.setBounds(120, 100, 150, 25);
+        add(txtCurso);
+
+        JLabel lblFecha = new JLabel("Fecha:");
+        lblFecha.setBounds(20, 140, 80, 25);
+        add(lblFecha);
+
+        txtFecha = new JTextField();
+        txtFecha.setBounds(120, 140, 150, 25);
+        add(txtFecha);
+
+        JLabel lblEstado = new JLabel("Estado:");
+        lblEstado.setBounds(20, 180, 80, 25);
+        add(lblEstado);
+
+        cbEstado = new JComboBox<>(new String[]{"Pendiente", "En proceso", "Completada"});
+        cbEstado.setBounds(120, 180, 150, 25);
+        add(cbEstado);
+
+        btnRegistrar = new JButton("Registrar");
+        btnRegistrar.setBounds(120, 230, 100, 30);
+        add(btnRegistrar);
+    }
+}
